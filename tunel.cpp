ce674cd93e758cab431650a9066f046a3cbe873e
tomaszmjurek <tomaszmjurek@gmail.com>
2020-06-27 13:48:46 +0200

    fix sprawdzanie miejsca w tunelu

--- tunel.cpp
+++ tunel.cpp
@@ -12,12 +12,12 @@ Tunel * tunele; /* zmienna globalna */
 int znajdzMiejsceWTunelu(kierunki gdzie) {
     int nrTunelu = id_proc % LICZBA_TUNELI;
     if (tunele[nrTunelu].kierunek == brak || tunele[nrTunelu].kierunek == gdzie)
-        if (tunele[nrTunelu].zajetosc < POJEMNOSC_TUNELU)
+        if (tunele[nrTunelu].zajetosc + ROZMIAR_EKIPY <= POJEMNOSC_TUNELU)
             return nrTunelu;
     for (int i = 0; i < LICZBA_TUNELI; i++) {
         nrTunelu = i;
         if (tunele[nrTunelu].kierunek == brak || tunele[nrTunelu].kierunek == gdzie)
-            if (tunele[nrTunelu].zajetosc < POJEMNOSC_TUNELU)
+            if (tunele[nrTunelu].zajetosc + ROZMIAR_EKIPY <= POJEMNOSC_TUNELU)
                 return nrTunelu;
     }
     return -1;
